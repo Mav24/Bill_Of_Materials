80b219b7f1d8a810a6aa7545154070df6f9e77c0
Maverick <maverick@sfosquad.com>
2019-09-05 14:23:24 -0400

    Download NuGet Reporting package, also add printing winform

---
+++ ValcomDrawings/PrintingMainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValcomDrawings
+{
+    public partial class PrintingMainForm : Form
+    {
+        public PrintingMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintingMainForm_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
